d7aca8ff7082b99ce9345e40b91cef8b9283bcd6
mshabiola <mshdabiola@gmal.com>
2022-10-27 19:02:57 +0100

    build: add spotless

--- model/src/main/java/com/mshdabiola/ludo/model/Counter.kt
+++ model/src/main/java/com/mshdabiola/ludo/model/Counter.kt
@@ -1,5 +1,8 @@
 package com.mshdabiola.ludo.model
 
-data class Counter(val id: Int = 0, val isEnable: Boolean = false, val isTotal: Boolean = false,
+data class Counter(
+    val id: Int = 0,
+    val isEnable: Boolean = false,
+    val isTotal: Boolean = false,
     val number: Int = 0
 )
