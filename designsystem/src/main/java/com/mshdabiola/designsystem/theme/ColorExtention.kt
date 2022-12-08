1c1636566515ad0f11b1eeff1bfe59ed1d15041f
mshabiola <mshdabiola@gmal.com>
2022-12-08 19:37:33 +0100

    fix: change board color and pawn

--- designsystem/src/main/java/com/mshdabiola/designsystem/theme/ColorExtention.kt
+++ designsystem/src/main/java/com/mshdabiola/designsystem/theme/ColorExtention.kt
@@ -8,12 +8,11 @@ import com.mshdabiola.ludo.model.GameColor
 @Composable
 fun GameColor.toHomeColor(): Color {
 
-    val isDarkMode = isSystemInDarkTheme()
     return when (this) {
-        GameColor.RED -> if (isDarkMode) dark_redContainer else red
-        GameColor.YELLOW -> if (isDarkMode) dark_yellowContainer else yellow
-        GameColor.BLUE -> if (isDarkMode) dark_blueContainer else blue
-        GameColor.GREEN -> if (isDarkMode) dark_greenContainer else green
+        GameColor.RED -> redBoard
+        GameColor.YELLOW -> yellowBoard
+        GameColor.BLUE -> blueBoard
+        GameColor.GREEN -> greenBoard
     }
 }
 
@@ -22,21 +21,21 @@ fun GameColor.toPawnColor(): Color {
 
     val isDarkMode = isSystemInDarkTheme()
     return when (this) {
-        GameColor.RED -> if (isDarkMode) dark_onred else light_red
-        GameColor.YELLOW -> if (isDarkMode) dark_onyellow else light_yellow
-        GameColor.BLUE -> if (isDarkMode) dark_onblue else light_blue
-        GameColor.GREEN -> if (isDarkMode) dark_ongreen else light_green
+        GameColor.RED -> red
+        GameColor.YELLOW -> yellow
+        GameColor.BLUE -> blue
+        GameColor.GREEN -> green
     }
 }
 
 @Composable
 fun GameColor.toPawnTextColor(): Color {
 
-    val isDarkMode = isSystemInDarkTheme()
-    return when (this) {
-        GameColor.RED -> if (isDarkMode) dark_red else light_onred
-        GameColor.YELLOW -> if (isDarkMode) dark_yellow else light_onyellow
-        GameColor.BLUE -> if (isDarkMode) dark_blue else light_onblue
-        GameColor.GREEN -> if (isDarkMode) dark_green else light_ongreen
-    }
+    return Color.White
+//    when (this) {
+//        GameColor.RED -> if (isDarkMode) dark_red else light_onred
+//        GameColor.YELLOW -> if (isDarkMode) dark_yellow else light_onyellow
+//        GameColor.BLUE -> if (isDarkMode) dark_blue else light_onblue
+//        GameColor.GREEN -> if (isDarkMode) dark_green else light_ongreen
+//    }
 }
