f055cb5794e5850aeff728729c379ff94a0bdd07
mshabiola <mshdabiola@gmal.com>
2023-07-17 13:37:56 +0100

    finish v2.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
             // who clones the code to sign and run the release variant, use the debug signing key.
             // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
             // Todo: comment code before release
-            signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("release")
             applicationIdSuffix = BuildType.RELEASE.applicationIdSuffix
             versionNameSuffix = BuildType.RELEASE.versionNameSuffix
         }
