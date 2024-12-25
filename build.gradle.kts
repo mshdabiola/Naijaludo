6b2ba13277027809a5a43adf13690d6c7eda0a07
mshdabiola <mshdabiola@gmail.com>
2024-12-25 20:04:24 +0100

    refactor: project restructuring
    Restructured the project for better organization and modularity.
    - Updated `settings.gradle.kts` to reflect the new module structure.
    - Updated `.gitignore` to exclude unnecessary build files.
    - Updated `gradlew` to reflect project changes.
    - Updated `gradle.properties` for better performance and support of AndroidX.
    - Added a `conveyor.conf` file for desktop builds.
    - Added a `compose_compiler_config.conf` file for Compose Compiler stability.
    - Added a `.editorconfig` file for code style settings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,26 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.dependencyGuard) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
     alias(libs.plugins.gms) apply false
-//    alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.compose.gradlePlugin)
-    alias(libs.plugins.manes)
-    alias(libs.plugins.little)
+    alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.powerAssert) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.conveyor) apply false
+    alias(libs.plugins.screenshot) apply false
+    alias(libs.plugins.kover) apply false
 
-    alias(libs.plugins.androidx.baselineprofile) apply false
-
-}
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
 
 
 }
-
-
-//./gradlew versionCatalogUpdate
-versionCatalogUpdate {
-    // sort the catalog by key (default is true)
-    sortByKey.set(true)
-    // Referenced that are pinned are not automatically updated.
-    // They are also not automatically kept however (use keep for that).
-    keep {
-
-        // keep versions without any library or plugin reference
-        keepUnusedVersions.set(true)
-        // keep all libraries that aren't used in the project
-        keepUnusedLibraries.set(true)
-        // keep all plugins that aren't used in the project
-        keepUnusedPlugins.set(true)
-    }
-}
-
-
-//tasks.create("add_spot"){
-//    val proj=project.rootDir
-//
-//  //  doFirst{
-//        print("add spotless to git")
-//        val gitHooksDirectory = File("$proj/.git/hooks/")
-//        if (!gitHooksDirectory.exists()) gitHooksDirectory.mkdirs()
-//        File("$proj/.git/hooks", "pre-commit").writeText(
-//            """
-//    #!/bin/bash
-//    echo "Running spotless check"
-//    ./gradlew spotlessApply --init-script gradle/init.gradle.kts
-//"""
-//        )
-//
-//        exec {
-//            commandLine("chmod","+x", ".git/hooks/pre-commit")
-//        }
-//
-//  //  }
-//
-//}
-//.gradlew add_spot
-
-//./gradlew assembleDebug -PenableComposeCompilerReports=true
-// ./gradlew assembleDebug -PenableComposeCompilerMetrics=true
