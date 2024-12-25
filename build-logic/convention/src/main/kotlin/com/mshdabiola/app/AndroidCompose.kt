58e1f2aa447e364276c6fd59300ca83addcd4857
mshdabiola <mshdabiola@gmail.com>
2024-12-25 19:30:56 +0100

    refactor(build-logic): update to new android studio FlamingoUpdated dependencies and configurations for compatibility with Android Studio Flamingo. Updated the compose options. Updated Kotlin and Java target versions. Updated the build process. Refactored the code for better organization and readability. Updated build files with new plugins and configurations. Added support for Google Play Store and FOSS builds using different product flavors and application IDs. Implemented badging tasks for APK validation. Improved the handling of Gradle-managed devices for testing. Disabled unnecessary Android instrumented tests for projects without androidTest sources. Updated the README and removed unnecessary files.

--- build-logic/convention/src/main/kotlin/com/mshdabiola/app/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/mshdabiola/app/AndroidCompose.kt
@@ -1,87 +1,65 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.mshdabiola.app
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import java.io.File
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
 
 /**
  * Configure Compose-specific options
  */
 internal fun Project.configureAndroidCompose(
-    commonExtension: CommonExtension<*, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
-    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
     commonExtension.apply {
         buildFeatures {
             compose = true
         }
 
-        composeOptions {
-            kotlinCompilerExtensionVersion =
-                libs.findVersion("androidxComposeCompiler").get().toString()
-        }
-
-//        kotlinOptions {
-//            freeCompilerArgs = freeCompilerArgs + buildComposeMetricsParameters()
+//        composeOptions {
+//            kotlinCompilerExtensionVersion =
+//                libs.findVersion("androidxComposeCompiler").get().toString()
 //        }
 
-        dependencies {
-            add("implementation", libs.findLibrary("androidx.compose.material3").get())
-            add(
-                "implementation",
-                libs.findLibrary("androidx-compose-material3-windowSizeClass").get()
-            )
-            add("implementation", libs.findLibrary("androidx-compose-ui").get())
-            add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
-            add("implementation", libs.findLibrary("androidx-lifecycle-runtimeCompose").get())
-            add("implementation", libs.findLibrary("androidx.compose.material3").get())
 
-            add("debugImplementation", libs.findLibrary("androidx-compose-ui-testManifest").get())
-            add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
+        dependencies {
+//            val bom = libs.findLibrary("androidx-compose-bom").get()
+//            add("implementation", platform(bom))
+//            add("androidTestImplementation", platform(bom))
+//            add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+//            add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
+        }
+
+        testOptions {
+            unitTests {
+                // For Robolectric
+                isIncludeAndroidResources = true
+            }
         }
     }
-}
 
-private fun Project.buildComposeMetricsParameters(): List<String> {
-    val metricParameters = mutableListOf<String>()
-    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
-    val enableMetrics = (enableMetricsProvider.orNull == "true")
-    if (enableMetrics) {
-        val metricsFolder = File(project.buildDir, "compose-metrics")
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
-        )
-    }
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+        fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
+        fun Provider<*>.relativeToRootProject(dir: String) = flatMap {
+            rootProject.layout.buildDirectory.dir(projectDir.toRelativeString(rootDir))
+        }.map { it.dir(dir) }
 
-    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
-    val enableReports = (enableReportsProvider.orNull == "true")
-    if (enableReports) {
-        val reportsFolder = File(project.buildDir, "compose-reports")
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
-        )
+        project.providers.gradleProperty("enableComposeCompilerMetrics").onlyIfTrue()
+            .relativeToRootProject("compose-metrics")
+            .let(metricsDestination::set)
+
+        project.providers.gradleProperty("enableComposeCompilerReports").onlyIfTrue()
+            .relativeToRootProject("compose-reports")
+            .let(reportsDestination::set)
+
+        stabilityConfigurationFile.set(rootProject.layout.projectDirectory.file("compose_compiler_config.conf"))
+
+        featureFlags.set(listOf(ComposeFeatureFlag.StrongSkipping))
     }
-    return metricParameters.toList()
 }
