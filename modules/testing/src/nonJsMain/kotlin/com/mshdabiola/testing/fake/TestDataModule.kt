86275319774926228bfd2c30fafe3bec90c4a7a5
mshdabiola <mshdabiola@gmail.com>
2024-12-28 15:22:08 +0100

    feat(testing): add fake implementations for p2p manager, sound, and store
    Added fake implementations for the following classes:
    - `P2pManager`
    - `SoundInterface`
    - `Store`
    
    These implementations will be used for testing purposes.

--- modules/testing/src/nonJsMain/kotlin/com/mshdabiola/testing/fake/TestDataModule.kt
+++ modules/testing/src/nonJsMain/kotlin/com/mshdabiola/testing/fake/TestDataModule.kt
@@ -6,6 +6,10 @@ package com.mshdabiola.testing.fake
 
 import com.mshdabiola.analytics.di.analyticsModule
 import com.mshdabiola.data.repository.UserDataRepository
+import com.mshdabiola.datastore.Store
+import com.mshdabiola.naijaludo.model.IP2pManager
+import com.mshdabiola.naijaludo.model.SoundInterface
+import com.mshdabiola.testing.datastore.FakeStore
 import com.mshdabiola.testing.di.testDispatcherModule
 import com.mshdabiola.testing.fake.repository.FakeUserDataRepository
 import org.koin.core.module.dsl.singleOf
@@ -15,4 +19,9 @@ import org.koin.dsl.module
 val testDataModule = module {
     includes(testDispatcherModule, analyticsModule)
     singleOf(::FakeUserDataRepository) bind UserDataRepository::class
+    singleOf(::FakeSound) bind SoundInterface::class
+    singleOf(::FakeP2pManager) bind IP2pManager::class
+    single {
+        FakeStore()
+    } bind Store::class
 }
