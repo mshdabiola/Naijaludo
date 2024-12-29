af589dee6d59185f00920fa9ca63d0619047dec1
mshdabiola <mshdabiola@gmail.com>
2024-12-29 15:03:26 +0100

    refactor(setting): update setting screen
    Updated the Setting screen to use a DialogUi component for a more streamlined user experience.
    Removed the previous OptionsDialog and MainContent composables, consolidating their functionality into the SettingScreen.
    Removed unused states and composables, simplifying the overall structure.
    Added mode setting to the basic setting section of the Setting screen.

--- features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingDialogUi.kt
+++ features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingDialogUi.kt
@@ -1,25 +1,17 @@
 package com.mshdabiola.setting
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -29,195 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.DialogProperties
-import com.mshdabiola.designsystem.component.DialogUi
 import com.mshdabiola.designsystem.string.Dimensions.Base2
-import com.mshdabiola.ui.state.SettingUiState
-import naijaludo.features.setting.generated.resources.Res
-import naijaludo.features.setting.generated.resources.assistant
-import naijaludo.features.setting.generated.resources.basic_setting
-import naijaludo.features.setting.generated.resources.board_setting
-import naijaludo.features.setting.generated.resources.board_style
-import naijaludo.features.setting.generated.resources.board_type
-import naijaludo.features.setting.generated.resources.close
-import naijaludo.features.setting.generated.resources.game_setting
-import naijaludo.features.setting.generated.resources.level
-import naijaludo.features.setting.generated.resources.music
-import naijaludo.features.setting.generated.resources.pawn_num
-import naijaludo.features.setting.generated.resources.pawn_number
-import naijaludo.features.setting.generated.resources.rotate
-import naijaludo.features.setting.generated.resources.sound
-import naijaludo.features.setting.generated.resources.sound_setting
 import naijaludo.features.setting.generated.resources.style
-import naijaludo.features.setting.generated.resources.type
 import org.jetbrains.compose.resources.StringArrayResource
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringArrayResource
 import org.jetbrains.compose.resources.stringResource
-
-@Composable
-fun SettingDialog(
-    show: Boolean = false,
-    settingUiState: SettingUiState,
-    setSetting: (SettingUiState) -> Unit = {},
-    onDismissRequest: () -> Unit = {},
-    //  setLanguage: (Int) -> Unit = {},
-) {
-    AnimatedVisibility(visible = show) {
-        DialogUi(
-            modifier = Modifier.padding(vertical = Base2),
-            onDismissRequest = onDismissRequest,
-            title = { Text(text = stringResource(Res.string.game_setting)) },
-            cancelIcon = {
-                IconButton(onClick = onDismissRequest) {
-                    Icon(
-                        imageVector = Icons.Default.Clear,
-                        contentDescription = stringResource(Res.string.close),
-                    )
-                }
-            },
-            content = {
-                Column(
-                    modifier = Modifier.verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-
-                ) {
-                    Title(title = stringResource(Res.string.basic_setting))
-                    SettingContainer {
-//                        SettingItem2(title = R.string.language) { modi ->
-//                            ExposeBox(
-//                                modifier = modi,
-//                                current = basic.language,
-//                                onValueChange = {
-//                                    basicSettingChange(basic.copy(language = it))
-//                                    setLanguage(it)
-//                                },
-//                                stringArrayRes = R.array.language,
-//                            )
-//                        }
-                        SettingItem2(title = Res.string.level) { modifier ->
-                            ExposeBox(
-                                modifier = modifier,
-                                current = settingUiState.gameLevel,
-                                onValueChange = {
-                                    setSetting(settingUiState.copy(gameLevel = it))
-                                },
-                                stringArrayRes = Res.array.level,
-                            )
-                        }
-                        //            SettingItem(title = R.string.count) { modifier ->
-                        //                Switch(modifier = modifier, checked = basic.directCount, onCheckedChange = {
-                        //                    basicSettingChange(basic.copy(directCount = it))
-                        //                })
-                        //            }
-                        SettingItem(title = Res.string.assistant) { modifier ->
-                            Switch(
-                                modifier = modifier,
-                                checked = settingUiState.assistant,
-                                onCheckedChange = {
-                                    setSetting(settingUiState.copy(assistant = it))
-                                },
-                            )
-                        }
-                    }
-                    Title(title = stringResource(Res.string.sound_setting))
-                    SettingContainer {
-                        SettingItem(title = Res.string.sound) { modifier ->
-                            Switch(
-                                modifier = modifier,
-                                checked = settingUiState.sound,
-                                onCheckedChange = {
-                                    setSetting(settingUiState.copy(sound = it))
-                                },
-                            )
-                        }
-                        SettingItem(title = Res.string.music) { modifier ->
-                            Switch(
-                                modifier = modifier,
-                                checked = settingUiState.music,
-                                onCheckedChange = {
-                                    setSetting(settingUiState.copy(music = it))
-                                },
-                            )
-                        }
-                    }
-
-                    Title(title = stringResource(Res.string.board_setting))
-                    SettingContainer {
-                        SettingItem2(Res.string.style) { modi ->
-                            ExposeBox(
-                                modifier = modi,
-                                current = settingUiState.boardStyle,
-                                onValueChange = {
-                                    setSetting(settingUiState.copy(boardStyle = it))
-                                },
-                                stringArrayRes = Res.array.board_style,
-                            )
-                        }
-                        SettingItem2(Res.string.type) { modi ->
-                            ExposeBox(
-                                modifier = modi,
-                                current = settingUiState.boardType,
-                                onValueChange = {
-                                    setSetting(settingUiState.copy(boardType = it))
-                                },
-                                stringArrayRes = Res.array.board_type,
-                            )
-                        }
-                        SettingItem(Res.string.pawn_num) { modi ->
-                            val value = stringArrayResource(Res.array.pawn_number)
-                            ExposeBox(
-                                modifier = modi.weight(0.5f),
-                                current = value.indexOfFirst { it == "${settingUiState.pawnNumber}" },
-                                onValueChange = {
-                                    setSetting(settingUiState.copy(pawnNumber = value[it].toInt()))
-                                },
-                                stringArrayRes = Res.array.pawn_number,
-                            )
-
-                            //
-                        }
-                        SettingItem(Res.string.rotate) { modifier ->
-                            Switch(
-                                modifier = modifier,
-                                checked = settingUiState.rotate,
-                                onCheckedChange = {
-                                    setSetting(settingUiState.copy(rotate = it))
-                                },
-                            )
-                        }
-                    }
-                }
-            },
-            properties = DialogProperties(),
-        )
-    }
-}
-//
-// @Preview(device = "spec:width=1280dp,height=800dp,dpi=480")
-// @Composable
-// fun DialogSettingPreview() {
-//    LudoAppTheme {
-//        Column(Modifier.fillMaxSize()) {
-//            SettingDialog(
-//                show = true,
-//                settingUiState = SettingUiState(
-//                    language = 0,
-//                    gameLevel = 0,
-//                    assistant = false,
-//                    boardType = 0,
-//                    boardStyle = 0,
-//                    pawnNumber = 0,
-//                    rotate = false,
-//                    sound = false,
-//                    music = false,
-//                    musicType = 0,
-//                )
-//
-//            )
-//        }
-//    }
-// }
 
 @Composable
 fun Title(title: String) {
--- features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingViewModel.kt
+++ features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingViewModel.kt
@@ -6,71 +6,23 @@ package com.mshdabiola.setting
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.mshdabiola.data.repository.UserDataRepository
 import com.mshdabiola.datastore.Store
-import com.mshdabiola.model.DarkThemeConfig
-import com.mshdabiola.model.ThemeBrand
 import com.mshdabiola.naijaludo.model.Setting
 import com.mshdabiola.ui.state.SettingUiState
 import com.mshdabiola.ui.state.toSetting
 import com.mshdabiola.ui.state.toUi
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-class SettingViewModel constructor(
-    private val userDataRepository: UserDataRepository,
+class SettingViewModel(
     private val setting: Store,
 ) : ViewModel() {
-
-    private val _settingState = MutableStateFlow<SettingState>(SettingState.Loading())
-    val settingState = _settingState.asStateFlow()
 
     val settingUiState = setting.setting
         .map { it.toUi() }
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(1000), Setting.default.toUi())
-
-    init {
-        update()
-    }
-
-    fun setThemeBrand(themeBrand: ThemeBrand) {
-        viewModelScope.launch {
-            _settingState.value = SettingState.Loading()
-
-            userDataRepository.setThemeBrand(themeBrand)
-
-            update()
-        }
-    }
-
-    /**
-     * Sets the desired dark theme config.
-     */
-    fun setDarkThemeConfig(darkThemeConfig: DarkThemeConfig) {
-        viewModelScope.launch {
-            _settingState.value = SettingState.Loading()
-
-            userDataRepository.setDarkThemeConfig(darkThemeConfig)
-
-            update()
-        }
-    }
-
-    private fun update() {
-        viewModelScope.launch {
-            _settingState.value = userDataRepository.userData.map {
-                SettingState.Success(
-                    themeBrand = it.themeBrand,
-                    darkThemeConfig = it.darkThemeConfig,
-                )
-            }.first()
-        }
-    }
 
     fun setSetting(settingUiState: SettingUiState) {
         viewModelScope.launch {
