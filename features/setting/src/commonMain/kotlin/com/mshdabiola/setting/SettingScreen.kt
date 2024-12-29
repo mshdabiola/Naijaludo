dbc22ca19487c29831585d9cfa27f833ac97f24f
mshdabiola <mshdabiola@gmail.com>
2024-12-29 15:03:26 +0100

    refactor(setting): update setting screen
    Updated the Setting screen to use a DialogUi component for a more streamlined user experience.
    Removed the previous OptionsDialog and MainContent composables, consolidating their functionality into the SettingScreen.
    Removed unused states and composables, simplifying the overall structure.
    Added mode setting to the basic setting section of the Setting screen.

--- features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingScreen.kt
+++ features/setting/src/commonMain/kotlin/com/mshdabiola/setting/SettingScreen.kt
@@ -4,44 +4,25 @@
 
 package com.mshdabiola.setting
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.outlined.Cancel
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mshdabiola.designsystem.component.DialogUi
 import com.mshdabiola.designsystem.string.Dimensions.Base2
 import com.mshdabiola.model.DarkThemeConfig
-import com.mshdabiola.model.ThemeBrand
-import com.mshdabiola.ui.Waiting
 import com.mshdabiola.ui.state.SettingUiState
 import naijaludo.features.setting.generated.resources.Res
 import naijaludo.features.setting.generated.resources.assistant
@@ -53,6 +34,7 @@ import naijaludo.features.setting.generated.resources.close
 import naijaludo.features.setting.generated.resources.daynight
 import naijaludo.features.setting.generated.resources.game_setting
 import naijaludo.features.setting.generated.resources.level
+import naijaludo.features.setting.generated.resources.mode
 import naijaludo.features.setting.generated.resources.music
 import naijaludo.features.setting.generated.resources.pawn_num
 import naijaludo.features.setting.generated.resources.pawn_number
@@ -60,12 +42,9 @@ import naijaludo.features.setting.generated.resources.rotate
 import naijaludo.features.setting.generated.resources.sound
 import naijaludo.features.setting.generated.resources.sound_setting
 import naijaludo.features.setting.generated.resources.style
-import naijaludo.features.setting.generated.resources.theme
 import naijaludo.features.setting.generated.resources.type
 import org.jetbrains.compose.resources.stringArrayResource
 import org.jetbrains.compose.resources.stringResource
-
-// import org.koin.androidx.compose.koinViewModel
 
 @Composable
 internal fun SettingRoute(
@@ -74,7 +53,6 @@ internal fun SettingRoute(
     onBack: () -> Unit,
     viewModel: SettingViewModel,
 ) {
-    val settingState = viewModel.settingState.collectAsStateWithLifecycle()
     val settingUiState = viewModel.settingUiState.collectAsStateWithLifecycle()
 
     SettingScreen(
@@ -82,118 +60,18 @@ internal fun SettingRoute(
         onDismissRequest = onBack,
         setSetting = viewModel::setSetting,
         settingUiState = settingUiState.value,
-//        settingState = settingState.value,
-//        setTheme = viewModel::setThemeBrand,
-//        setDarkMode = viewModel::setDarkThemeConfig,
-//        onBack = onBack,
     )
 }
 
 @Composable
 internal fun SettingScreen(
-    settingState: SettingState,
-    modifier: Modifier = Modifier,
-    setTheme: (ThemeBrand) -> Unit = {},
-    setDarkMode: (DarkThemeConfig) -> Unit = {},
-    onBack: () -> Unit = {},
-) {
-    Card(modifier = modifier.testTag("setting:screen")) {
-        AnimatedContent(settingState) {
-            when (it) {
-                is SettingState.Loading -> Waiting(modifier)
-                is SettingState.Success -> MainContent(
-                    modifier = modifier,
-                    settingState = it,
-                    setTheme = setTheme,
-                    setDarkMode = setDarkMode,
-                    onBack = onBack,
-                )
-
-                else -> {}
-            }
-        }
-    }
-}
-
-@Composable
-internal fun MainContent(
-    modifier: Modifier = Modifier,
-    settingState: SettingState.Success,
-    setTheme: (ThemeBrand) -> Unit = {},
-    setDarkMode: (DarkThemeConfig) -> Unit = {},
-    onBack: () -> Unit = {},
-) {
-    var dark by remember { mutableStateOf(false) }
-    var theme by remember { mutableStateOf(false) }
-    val themeArray = stringArrayResource(Res.array.theme)
-    val dayLightArray = stringArrayResource(Res.array.daynight)
-
-    Column(
-        modifier.padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(text = "Settings", style = MaterialTheme.typography.titleLarge)
-            IconButton(
-                onClick = onBack,
-
-            ) {
-                Icon(imageVector = Icons.Outlined.Cancel, "cancel")
-            }
-        }
-
-        Spacer(Modifier.height(8.dp))
-
-        ListItem(
-            modifier = Modifier.testTag("setting:theme").clickable { theme = true },
-            headlineContent = { Text("Theme") },
-            supportingContent = {
-                Text(themeArray.getOrNull(settingState.themeBrand.ordinal) ?: "")
-            },
-        )
-
-        ListItem(
-            modifier = Modifier.testTag("setting:mode").clickable { dark = true },
-            headlineContent = { Text("DayNight mode") },
-            supportingContent = {
-                Text(dayLightArray.getOrNull(settingState.darkThemeConfig.ordinal) ?: "")
-            },
-        )
-    }
-
-    AnimatedVisibility(theme) {
-        OptionsDialog(
-            modifier = Modifier,
-            options = themeArray,
-            current = settingState.themeBrand.ordinal,
-            onDismiss = { theme = false },
-            onSelect = { setTheme(ThemeBrand.entries[it]) },
-        )
-    }
-    AnimatedVisibility(dark) {
-        OptionsDialog(
-            modifier = Modifier,
-            options = dayLightArray,
-            current = settingState.darkThemeConfig.ordinal,
-            onDismiss = { dark = false },
-            onSelect = { setDarkMode(DarkThemeConfig.entries[it]) },
-        )
-    }
-}
-
-@Composable
-fun SettingScreen(
     modifier: Modifier = Modifier,
     settingUiState: SettingUiState,
     setSetting: (SettingUiState) -> Unit = {},
     onDismissRequest: () -> Unit = {},
 ) {
     DialogUi(
-        modifier = Modifier.padding(vertical = Base2),
+        modifier = modifier.padding(vertical = Base2),
         title = { Text(text = stringResource(Res.string.game_setting)) },
         cancelIcon = {
             IconButton(onClick = onDismissRequest) {
@@ -211,17 +89,6 @@ fun SettingScreen(
             ) {
                 Title(title = stringResource(Res.string.basic_setting))
                 SettingContainer {
-//                        SettingItem2(title = R.string.language) { modi ->
-//                            ExposeBox(
-//                                modifier = modi,
-//                                current = basic.language,
-//                                onValueChange = {
-//                                    basicSettingChange(basic.copy(language = it))
-//                                    setLanguage(it)
-//                                },
-//                                stringArrayRes = R.array.language,
-//                            )
-//                        }
                     SettingItem2(title = Res.string.level) { modifier ->
                         ExposeBox(
                             modifier = modifier,
@@ -232,11 +99,16 @@ fun SettingScreen(
                             stringArrayRes = Res.array.level,
                         )
                     }
-                    //            SettingItem(title = R.string.count) { modifier ->
-                    //                Switch(modifier = modifier, checked = basic.directCount, onCheckedChange = {
-                    //                    basicSettingChange(basic.copy(directCount = it))
-                    //                })
-                    //            }
+                    SettingItem2(title = Res.string.mode) { modifier ->
+                        ExposeBox(
+                            modifier = modifier,
+                            current = settingUiState.darkThemeConfig.ordinal,
+                            onValueChange = {
+                                setSetting(settingUiState.copy(darkThemeConfig = DarkThemeConfig.entries[it]))
+                            },
+                            stringArrayRes = Res.array.daynight,
+                        )
+                    }
                     SettingItem(title = Res.string.assistant) { modifier ->
                         Switch(
                             modifier = modifier,
