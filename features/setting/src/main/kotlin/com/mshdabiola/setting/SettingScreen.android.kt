be36f26e515e7996623b4e880226dfa1f93b3453
mshdabiola <mshdabiola@gmail.com>
2024-12-29 15:42:04 +0100

    refactor(setting): update setting screen to use ui state
    Updated the SettingScreen to use ui state instead of setting state.
    This change ensures that the screen is updated correctly when the setting state changes.

--- features/setting/src/main/kotlin/com/mshdabiola/setting/SettingScreen.android.kt
+++ features/setting/src/main/kotlin/com/mshdabiola/setting/SettingScreen.android.kt
@@ -1,10 +1,7 @@
 package com.mshdabiola.setting
 
-import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.mshdabiola.model.DarkThemeConfig
-import com.mshdabiola.model.ThemeBrand
 import com.mshdabiola.naijaludo.model.Setting
 import com.mshdabiola.ui.state.toUi
 
@@ -14,5 +11,5 @@ fun ScreenPreview() {
     SettingScreen(
         settingUiState = Setting.default.toUi(),
 
-        )
+    )
 }
