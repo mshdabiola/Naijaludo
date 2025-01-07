6c0186e2c4c022c9026bae2a68000fb95549c9d5
mshdabiola <mshdabiola@gmail.com>
2025-01-08 00:34:38 +0100

    feat: add screenshots test for game screen
    Added screenshot tests for the game screen in both light and dark themes across various device configurations.
    
    The new screenshot tests cover:
    
    -   Loading screen (light and dark themes)
    -   Main screen (light and dark themes)
    
    These tests are set up to run on different device configurations, ensuring the UI is consistent across devices.

--- features/game/src/screenshotTest/kotlin/com/mshdabiola/game/ScreenScreenshotTests.kt
+++ features/game/src/screenshotTest/kotlin/com/mshdabiola/game/ScreenScreenshotTests.kt
@@ -4,74 +4,76 @@
 
 package com.mshdabiola.game
 
-import androidx.compose.material3.Text
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.mshdabiola.designsystem.theme.darkDefaultScheme
+import com.mshdabiola.designsystem.theme.lightDefaultScheme
+import com.mshdabiola.naijaludo.model.Constant
+import com.mshdabiola.ui.DevicePreviews
+import com.mshdabiola.ui.SharedContentPreview
+import com.mshdabiola.ui.state.toLudoUiState
 
 class ScreenScreenshotTests {
 
-    @Preview
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
     @Composable
-    private fun Test() {
-        Text(text = "Test")
+    fun LoadingLight() {
+        MaterialTheme(colorScheme = lightDefaultScheme) {
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    GameScreen(
+                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                    )
+                }
+            }
+        }
     }
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun LoadingLight() {
-//        MaterialTheme(colorScheme = lightDefaultScheme) {
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    GameScreen(
-//                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                    )
-//                }
-//            }
-//        }
-//    }
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun LoadingDark() {
-//        MaterialTheme(colorScheme = darkDefaultScheme) {
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    GameScreen(
-//                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                    )
-//                }
-//            }
-//        }
-//    }
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun MainLight() {
-//        MaterialTheme(colorScheme = lightDefaultScheme) {
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    GameScreen(
-//                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                    )
-//                }
-//            }
-//        }
-//    }
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun MainDark() {
-//        MaterialTheme(colorScheme = darkDefaultScheme) {
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    GameScreen(
-//                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                    )
-//                }
-//            }
-//        }
-//    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun LoadingDark() {
+        MaterialTheme(colorScheme = darkDefaultScheme) {
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    GameScreen(
+                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                    )
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun MainLight() {
+        MaterialTheme(colorScheme = lightDefaultScheme) {
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    GameScreen(
+                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                    )
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun MainDark() {
+        MaterialTheme(colorScheme = darkDefaultScheme) {
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    GameScreen(
+                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                    )
+                }
+            }
+        }
+    }
 }
