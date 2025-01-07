663bb416b344da0df0e0cf8f7bacc61348a7cc59
mshdabiola <mshdabiola@gmail.com>
2025-01-07 16:56:36 +0100

    feat: add screenshot tests for game screen
    Added screenshot tests for the game screen in both dark and light themes and different device configurations:
    
    -   Loading screen (dark and light themes)
    -   Main screen (dark and light themes)
    
    The screenshots were added for the following device configurations:
    
    -   Foldable
    -   Landscape
    -   Phone
    -   Tablet

--- features/game/src/screenshotTest/kotlin/com/mshdabiola/game/ScreenScreenshotTests.kt
+++ features/game/src/screenshotTest/kotlin/com/mshdabiola/game/ScreenScreenshotTests.kt
@@ -4,85 +4,97 @@
 
 package com.mshdabiola.game
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import com.mshdabiola.designsystem.theme.darkDefaultScheme
+import com.mshdabiola.designsystem.theme.lightDefaultScheme
+import com.mshdabiola.naijaludo.model.Constant
+import com.mshdabiola.ui.DevicePreviews
+import com.mshdabiola.ui.SharedContentPreview
+import com.mshdabiola.ui.state.toLudoUiState
+
 class ScreenScreenshotTests {
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun LoadingLight() {
-//        MaterialTheme(colorScheme = lightDefaultScheme) {
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    val windowAdaptiveInfo = currentWindowAdaptiveInfo()
-//                    val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
-//                    Surface {
-//                        SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                            GameScreen(
-//                                deviceType = state.getDevietype(),
-//                                gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun LoadingDark() {
-//        MaterialTheme(colorScheme = darkDefaultScheme) {
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    val windowAdaptiveInfo = currentWindowAdaptiveInfo()
-//                    val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
-//                    Surface {
-//                        SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                            GameScreen(
-//                                deviceType = state.getDevietype(),
-//                                gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun MainLight() {
-//        MaterialTheme(colorScheme = lightDefaultScheme) {
-//            val windowAdaptiveInfo = currentWindowAdaptiveInfo()
-//            val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    GameScreen(
-//                        deviceType = state.getDevietype(),
-//                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                    )
-//                }
-//            }
-//        }
-//    }
-//
-//    @OptIn(ExperimentalSharedTransitionApi::class)
-//    @DevicePreviews
-//    @Composable
-//    fun MainDark() {
-//        MaterialTheme(colorScheme = darkDefaultScheme) {
-//            val windowAdaptiveInfo = currentWindowAdaptiveInfo()
-//            val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
-//            Surface {
-//                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
-//                    GameScreen(
-//                        deviceType = state.getDevietype(),
-//                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
-//                    )
-//                }
-//            }
-//        }
-//    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun LoadingLight() {
+        MaterialTheme(colorScheme = lightDefaultScheme) {
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    val windowAdaptiveInfo = currentWindowAdaptiveInfo()
+                    val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
+                    Surface {
+                        SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                            GameScreen(
+                                deviceType = state.getDevietype(),
+                                gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun LoadingDark() {
+        MaterialTheme(colorScheme = darkDefaultScheme) {
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    val windowAdaptiveInfo = currentWindowAdaptiveInfo()
+                    val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
+                    Surface {
+                        SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                            GameScreen(
+                                deviceType = state.getDevietype(),
+                                gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun MainLight() {
+        MaterialTheme(colorScheme = lightDefaultScheme) {
+            val windowAdaptiveInfo = currentWindowAdaptiveInfo()
+            val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    GameScreen(
+                        deviceType = state.getDevietype(),
+                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                    )
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @DevicePreviews
+    @Composable
+    fun MainDark() {
+        MaterialTheme(colorScheme = darkDefaultScheme) {
+            val windowAdaptiveInfo = currentWindowAdaptiveInfo()
+            val state = NaijaLudoAppState(windowAdaptiveInfo.windowSizeClass)
+            Surface {
+                SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                    GameScreen(
+                        deviceType = state.getDevietype(),
+                        gameUiState = Constant.getDefaultGameState().toLudoUiState(),
+                    )
+                }
+            }
+        }
+    }
 }
