07620eeef6646838aa8c2d2187fcc48c6fc0be6c
mshdabiola <mshdabiola@gmail.com>
2025-02-27 19:55:07 +0100

    remove common module

--- common/naijaludo/src/main/java/com/mshdabiola/naijaludo/model/Counter.kt
+++
@@ -1,8 +0,0 @@
-package com.mshdabiola.naijaludo.model
-
-data class Counter(
-    val id: Int = 0,
-    val isEnable: Boolean = false,
-    val isTotal: Boolean = false,
-    val number: Int = 0,
-)
